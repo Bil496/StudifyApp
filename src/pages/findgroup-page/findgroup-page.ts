fdb21b55cb9712a6220fe82fac0c5090ad2a79f5
theonuryildiz <onuryildiz@windowslive.com>
2018-02-23 00:52:35 +0300

    package.json file modified for better use.
    figured out how to use http calls. theres an example at findgroup-page.ts

--- src/pages/findgroup-page/findgroup-page.ts
+++ src/pages/findgroup-page/findgroup-page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpModule, ResponseType } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @IonicPage()
 @Component({
@@ -29,10 +33,15 @@ export class FindGroupPage {
   ];
 
   items2;
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, http: HttpClient) {
 
+    var head = new HttpHeaders();
+    head.append('Access-Control-Allow-Origin' , '*');
+    head.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
+    head.append('Accept','application/json');
+    head.append('content-type','application/json');
     this.items2 = this.items;
-
+    http.get('http://localhost:8100/#/home', {headers: head, responseType: 'text'} ).pipe().subscribe(res => console.log(res.toString()));
   }
 
   delete(item) {
